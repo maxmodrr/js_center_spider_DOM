44e23f19aacf2839ebbb0d0af78b427faf56fea9
maxmodrr <maxmodrr@gmail.com>
2024-10-21 02:54:46 +0300

    make build

---
+++ index.b9ff3852.js
@@ -0,0 +1,2 @@
+var t=document.querySelector(".wall"),e=document.querySelector(".spider"),o=Math.round(t.offsetWidth-e.clientWidth)/2,c=Math.round(t.offsetHeight-e.clientHeight)/2;e.style.left="".concat(o,"px"),e.style.top="".concat(c,"px");
+//# sourceMappingURL=index.b9ff3852.js.map
