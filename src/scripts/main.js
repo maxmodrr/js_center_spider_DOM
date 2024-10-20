ec2c5bdfeb0cf6f0c8e3af8c5ae16e7b937b1707
maxmodrr <maxmodrr@gmail.com>
2024-10-21 02:50:19 +0300

    results

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
 // write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+spider.style.left =
+  Math.round(wall.offsetWidth - spider.clientWidth) / 2 + 'px';
+
+spider.style.top =
+  Math.round(wall.offsetHeight - spider.clientHeight) / 2 + 'px';
